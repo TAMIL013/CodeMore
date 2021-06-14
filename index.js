8cc98627be788aa2a54ec11aa61a6ef77e742eb2
TAMIL013 <tamilarasupsk@gmail.com>
2021-06-14 12:19:13 +0530

    CodeMore v0.0.1

---
+++ index.js
@@ -0,0 +1,21 @@
+var express=require('express');
+// const { spawn } = require('child_process');
+var app=express();
+
+
+app.get('/',(req,res)=>{
+    res.send("hello world")
+})
+app.get('/py',get_code_geeks)
+function get_code_geeks(req,res){
+    url = "https://practice.geeksforgeeks.org/explore/?page=1&category%5B%5D=Kadane"
+    var spawn = require("child_process").spawn;
+ 
+    var process = spawn('python',["./scraper.py",req.query.category]);
+    process.stdout.on('data', function(data) {
+        var d=data.toString();
+        res.send(d);
+    } )
+    // res.send(process)
+}
+app.listen(8000,()=>{console.log("listening on port 8000")})
---
+++ scraper.py
@@ -0,0 +1,48 @@
+
+import sys
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
+import re
+import json
+
+driver_path = "./chromedriver.exe"
+brave_path = "C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe"
+
+option = webdriver.ChromeOptions()
+option.binary_location = brave_path
+# option.add_argument("--incognito") OPTIONAL
+option.add_argument("--headless") 
+
+# Create new Instance of Chrome
+driver = webdriver.Chrome(executable_path=driver_path, chrome_options=option)
+
+
+All_code=[]
+def get_code_geeksforgeeks(url):  
+    driver.get(url) 
+    time.sleep(5) 
+    html = driver.page_source
+    soup = BeautifulSoup(html, "html.parser")
+
+    table=soup.find('div',{'id':'problemFeed'})
+    row=table.find_all('div',{'class':'problem-block'})
+    for cd in row:
+        link=cd.find('a', href=re.compile("problems"))
+        title=cd.find('span').text
+        code={
+            'title':title,
+            'link':link.get('href')
+        }
+        All_code.append(code)
+    # for i in All_code:
+    #     print(i)
+
+url = "https://practice.geeksforgeeks.org/explore/?page=1&category%5B%5D=Kadane"
+url1="https://practice.geeksforgeeks.org/explore/?page"
+url2="=1&category%5B%5D="
+# url=url1+url2+sys.argv[1]
+get_code_geeksforgeeks(url)
+# output=json.dumps(All_code,separators=(',',':'))
+# print(output)
+print(All_code)
