d22d9b0578ce6b495590136232a33f70ed68e294
TAMIL013 <tamilarasupsk@gmail.com>
2021-06-16 22:32:53 +0530

    v

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express=require('express');
 // const { spawn } = require('child_process');
 var app=express();
-
+var fs=require('fs');
 
 app.get('/',(req,res)=>{
     res.send("hello world")
@@ -11,11 +11,15 @@ function get_code_geeks(req,res){
     url = "https://practice.geeksforgeeks.org/explore/?page=1&category%5B%5D=Kadane"
     var spawn = require("child_process").spawn;
  
-    var process = spawn('python',["./scraper.py",req.query.category]);
-    process.stdout.on('data', function(data) {
-        var d=data.toString();
-        res.send(d);
-    } )
-    // res.send(process)
+    spawn('python',["./scraper.py",req.query.category]);
+    fs.readFile('./geeks.txt', 'utf8' , (err, data) => {
+        if (err) {
+          console.error(err)
+          return
+        }
+        console.log(data)
+        res.send(data)
+      })
+    
 }
-app.listen(process.env.PORT,()=>{console.log("listening on port 8000")})
+app.listen(8000 || process.env.PORT,()=>{console.log("listening on port 8000")})
--- scraper.py
+++ scraper.py
@@ -11,22 +11,30 @@ brave_path = "C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe
 
 option = webdriver.ChromeOptions()
 option.binary_location = brave_path
-# option.add_argument("--incognito") OPTIONAL
+# # option.add_argument("--incognito") OPTIONAL
 option.add_argument("--headless") 
 
-# Create new Instance of Chrome
+# # Create new Instance of Chrome
 driver = webdriver.Chrome(executable_path=driver_path, chrome_options=option)
 
 
 All_code=[]
+def WriteFile(li):
+    f=open("geeks.txt","a")
+    f.write(li)
+    f.close()
+
 def get_code_geeksforgeeks(url):  
     driver.get(url) 
-    time.sleep(5) 
+    # time.sleep(5) 
     html = driver.page_source
     soup = BeautifulSoup(html, "html.parser")
 
     table=soup.find('div',{'id':'problemFeed'})
     row=table.find_all('div',{'class':'problem-block'})
+    f=open("geeks.txt","w")
+    f.write("")
+    f.write("{")
     for cd in row:
         link=cd.find('a', href=re.compile("problems"))
         title=cd.find('span').text
@@ -35,6 +43,10 @@ def get_code_geeksforgeeks(url):
             'link':link.get('href')
         }
         All_code.append(code)
+        f.write("{'title':'"+title+"','link':'"+link.get('href')+"'}")
+    f.write("}")
+    f.close()
+    
     # for i in All_code:
     #     print(i)
 
@@ -45,4 +57,9 @@ url2="=1&category%5B%5D="
 get_code_geeksforgeeks(url)
 # output=json.dumps(All_code,separators=(',',':'))
 # print(output)
-print(All_code)
+
+# print(All_code)
+# WriteFile({'ddd'})
+f=open("geeks.txt","r")
+# print(f.read())
+
